f34d7c7c4b5ddd6788df297c7b64d31d8fe39cfd
NicolasOrtiz05 <115834594+NicolasOrtiz05@users.noreply.github.com>
2024-08-15 22:56:40 -0500

    prueba cliente

--- src/main/java/edu/unisabana/dyas/patterns/Client.java
+++ src/main/java/edu/unisabana/dyas/patterns/Client.java
@@ -7,7 +7,8 @@ import edu.unisabana.dyas.patterns.utils.AudioPlayer;
 public class Client {
     public static void main(String[] args) {
         AudioPlayer audioPlayer = new AdvancedAudioPlayerAdapter(new AdvancedAudioPlayer());
-        audioPlayer.play("mp4", "video.mp4");
+        audioPlayer.play("MP4", "video.mp4")
+        audioPlayer.play("VLC", "video.vlc")
         audioPlayer.stop();
     }
 }
