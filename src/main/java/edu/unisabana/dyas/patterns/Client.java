c3271c79a5c9be0d5fbc6767f4afe8269a2b1193
juan david sanchez <Juansangon@unisabana.edu.co>
2024-08-15 21:16:29 -0500

    modificacion

--- src/main/java/edu/unisabana/dyas/patterns/Client.java
+++ src/main/java/edu/unisabana/dyas/patterns/Client.java
@@ -1,6 +1,5 @@
 package edu.unisabana.dyas.patterns;
 
-import edu.unisabana.dyas.patterns.utils.AdvancedAudioPlayerAdapter;
 import edu.unisabana.dyas.patterns.utils.AdvancedAudioPlayer;
 import edu.unisabana.dyas.patterns.utils.AudioPlayer;
 
@@ -8,7 +7,7 @@ import edu.unisabana.dyas.patterns.utils.AudioPlayer;
 public class Client {
     public static void main(String[] args) {
         AudioPlayer audioPlayer = new AdvancedAudioPlayerAdapter(new AdvancedAudioPlayer());
-        audioPlayer.play("mp4", "video.mp4");
+        audioPlayer.play("MP4", "video.mp4");
         audioPlayer.stop();
     }
 }
